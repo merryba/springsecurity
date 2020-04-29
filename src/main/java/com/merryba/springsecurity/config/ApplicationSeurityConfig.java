6983bfc1f8d6454114f112a1db486af340e13b9e
merryba@gmail.com <merryba@gmail.com>
2020-04-28 21:28:28 -0700

    Basic Auth - First commit

---
+++ src/main/java/com/merryba/springsecurity/config/ApplicationSeurityConfig.java
@@ -0,0 +1,23 @@
+package com.merryba.springsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSeurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http
+			.authorizeRequests()
+				.anyRequest()
+				.authenticated()
+				.and()
+				.httpBasic();
+	}
+
+}
